4e2f953ef38dd5978362a0d0c2fbad5dfc2df5cd
Ethan Neece <34079301+Ethanneece@users.noreply.github.com>
2018-06-23 14:22:05 -0400

    Images Plus changes

--- src/game/Tama.java
+++ src/game/Tama.java
@@ -1,28 +1,29 @@
 package game;
 import java.awt.Dimension;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Tama {
+	
 	public static void showPet() {
 		JFrame show = new JFrame("Tamagachi");
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		ImageIcon img = new ImageIcon("C:/Users/Allison/OneDrive/programming/Tomogachi/egg icon.png");
+		show.setLayout(null);
+		ImageIcon img = new ImageIcon(Tama.class.getResource("Images/Egg.png"));
 		createSettings(show);
+		
+		System.out.println(img.getIconWidth() + " " + img.getIconHeight());
 		
 		show.setPreferredSize(new Dimension(216, 289));
 		show.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		show.setIconImage(img.getImage());
 		show.pack();
-		
+		show.setResizable(false);
 		createPets(show);
-		createFeedButton(show);
-		createCleanButton(show);
-		createResetButton(show);
+		createFeedButton(show); 
+		createCleanButton(show); 
+		createResetButton(show); 
 		//Buttons here = new Buttons(show);
 		//here.setSize(100, 100);
 		//here.setVisible(true);
@@ -31,38 +32,41 @@ public class Tama {
 		show.add(empty);
 		show.setVisible(true);
 		
+		
 	}
+	
 	public static void createPets(JFrame show) {
-		JLabel pet = new JLabel(new ImageIcon("C:/Users/Allison/OneDrive/programming/Tomogachi/egg.gif")); 
+		JLabel pet = new JLabel(new ImageIcon(Tama.class.getResource("Images/EggMoving.gif"))); 
 		pet.setBounds((show.getWidth()/2)-15, 170, 22, 27);
 		show.add(pet);
 	}
 	public static void createSettings(JFrame show) {
-		JLabel health = new JLabel(new ImageIcon("C:/Users/Allison/OneDrive/programming/Tomogachi/health.png")); 
-		show.add(health);
+		JLabel health = new JLabel(new ImageIcon(Tama.class.getResource("Images/HealthBar.png"))); 
 		health.setBounds(65, 35, 107, 7);
+		show.add(health);
 		
-		JLabel border = new JLabel(new ImageIcon("C:/Users/Allison/OneDrive/programming/Tomogachi/border.png"));
-		show.add(border);
+		JLabel border = new JLabel(new ImageIcon(Tama.class.getResource("Images/Border.png")));
 		border.setBounds((show.getWidth()/2), show.getHeight()/2, 205, 255);
+		show.add(border);
 	}
 	public static void createFeedButton(JFrame show) {
-		JButton feed = new JButton(new ImageIcon("C:/Users/Allison/OneDrive/programming/Tomogachi/feed button.png")); 
-		show.add(feed);
+		JButton feed = new JButton(new ImageIcon(Tama.class.getResource("Images/FeedButton.png"))); 
 		feed.setBounds(23, 227, 40, 15);
-		feed.setVisible(true);
+		show.add(feed);
 	}
 	public static void createCleanButton(JFrame show) {
-		JButton clean = new JButton(new ImageIcon("C:/Users/Allison/OneDrive/programming/Tomogachi/clean.png")); 
-		show.add(clean);
+		JButton clean = new JButton(new ImageIcon(Tama.class.getResource("Images/CleanButton.png"))); 
 		clean.setBounds(81, 227, 40, 15);
-		clean.setVisible(true);
+		show.add(clean);
 	}
 	public static void createResetButton(JFrame show) {
-		JButton reset =new JButton(new ImageIcon("C:/Users/Allison/OneDrive/programming/Tomogachi/reset.png")); 
-		show.add(reset);
+		JButton reset = new JButton(new ImageIcon(Tama.class.getResource("Images/ResetButton.png"))); 
 		reset.setBounds(136, 227, 40, 15);
-		reset.setVisible(true);
+		show.add(reset);
+	}
+	public static void reDrawWindow(JFrame show) {
+		show.revalidate(); 
+		show.repaint(); 
 	}
 	
 	public static void main(String[] args) {	

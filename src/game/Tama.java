dbe8466c07ba032441e0bd3794a093b8e34013cf
Ethan Neece <ethan.neece1@gmail.com>
2018-08-17 19:25:33 -0400

    did it

--- src/game/Tama.java
+++ src/game/Tama.java
@@ -3,9 +3,12 @@ package game;
 import javax.swing.JLabel;
 
 public class Tama {
+
+    private String name;
     private int food, health, petState;
     private JLabel tom;
-
+    private double exp;
+    private double[] expPerFood = { 10, 5, 1.5, .9};
 
 	public Tama() {
 	    food = 0;
@@ -14,12 +17,16 @@ public class Tama {
 		Window.showPet(this);
 	}
 
-    public int getFood() {
-        return food;
+    public String getName() {
+        return name;
     }
 
-    public void incrementFood(int food) {
-        this.food += food;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFood() {
+        return food;
     }
 
     public void setFood(int food) {
@@ -46,17 +53,35 @@ public class Tama {
         return petState;
     }
 
-
     public void setPetState(int petState) {
         this.petState = petState;
     }
 
-    public void incrementPetState(int petState) {
-        this.petState += petState;
+    public double getExp() {
+        return exp;
+    }
+
+    public void setExp(double exp) {
+        this.exp = exp;
     }
 
     public void grow() {
 	    this.petState += 1;
+	    this.exp = 0;
         Window.setTamaPicture(this);
+    }
+
+    public double[] getExpPerFood() {
+        return expPerFood;
+    }
+
+    public void setExpPerFood(double[] expPerFood) {
+        this.expPerFood = expPerFood;
+    }
+
+    public String toString() {
+	    return "Food: " + food + "\n" +
+                "Health" + health + "\n" +
+                "petState" + petState + "\n";
     }
 }

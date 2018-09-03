fa222ddf539a15a099d45fea4c59221292324c46
AllisanLu <alcp2610@gmail.com>
2018-09-03 17:18:53 -0400

    fixed reset merge issues :D

--- src/game/buttons/ResetButton.java
+++ src/game/buttons/ResetButton.java
@@ -2,10 +2,8 @@ package game.buttons;
 
 import game.Driver;
 import game.Tama;
-<<<<<<< HEAD
-=======
 import game.Window;
->>>>>>> b8a76296efdc213e470713049458187a82ef1b8a
+
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -20,11 +18,9 @@ public class ResetButton implements ActionListener {
     public void actionPerformed(ActionEvent arg0) {
         Tama currentTama = Driver.getTama();
         currentTama.reset();
-<<<<<<< HEAD
-=======
+
         Window.removePoop();
->>>>>>> b8a76296efdc213e470713049458187a82ef1b8a
-        System.out.println("THE RESET HAS BEEN PRESSED UR BABY HAS BEEN REVIVED DONT FUK UP AGAIN");
+        System.out.println("DERP");
     }
 }
 

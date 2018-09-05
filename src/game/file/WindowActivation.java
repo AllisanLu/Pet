7fe535b7a986c81b6bb46b13cfba79109c463a59
AllisanLu <alcp2610@gmail.com>
2018-09-04 23:02:52 -0400

    THE LOOP

---
+++ src/game/file/WindowActivation.java
@@ -0,0 +1,62 @@
+package game.file;
+
+import game.Driver;
+import game.Window;
+
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class WindowActivation implements WindowListener {
+
+    public WindowActivation() {
+        System.out.println("Windows Activated");
+        Window.getShow().addWindowListener(this);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        GameLoader.makeFile();
+        GameLoader.readFile();
+
+        Window.setTamaPicture(Driver.getTama());
+        //System.out.println("poop is " + Driver.getTama().getPoop());
+        for(int i = 0; i < Driver.getTama().getPoop(); i++){
+            Window.addPoop();
+            //TODO: add correct amount of poop, why does it only add one right now? btw the Driver.get poop() thing works
+        }
+
+        System.out.println("hi");
+    }
+    //hey so I'ms leave this here o/ bob says hi
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        new GameSaver(Driver.getTama());
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
